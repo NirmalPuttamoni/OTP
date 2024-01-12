f89a4900b2d4feed196fd1b2d9e6ccd79b2f433b
Nirmal <puttamoninirmal01@gmail.com>
2024-01-12 22:53:04 +0530

    Added new functionalities

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const inputRef = document.querySelectorAll('.otp input');
 const firstInputRef = document.querySelector('.otp input:nth-child(1)');
+const verifyButtonRef = document.querySelector('.otp-wrapper button');
+const generateOTPBtnRef = document.querySelector('.generate-otp button');
+const otpVerificationRef = document.querySelector('.otp-verification');
+const rejectRef = document.querySelector('.otp-verification .reject-box');
+const successRef = document.querySelector('.otp-verification .success-box');
+
 firstInputRef.focus();
 
 inputRef.forEach(input => {
@@ -16,4 +22,48 @@ inputRef.forEach(input => {
             e.target.previousElementSibling?.focus();
         }
     })
+})
+
+// generate OTP
+let generatedOTP = document.querySelector('.generate-otp span').innerText;
+// console.log(generatedOTP)
+
+generateOTPBtnRef.addEventListener('click' , e => {
+    const otpRef = document.querySelector('.generate-otp span')
+    const otp = generateOTp(10000,99999);
+    otpRef.innerText = otp;
+    generatedOTP=otp;
+})
+
+function generateOTp(min, max){
+    const otp = Math.floor(min + (Math.random() * (max - min + 1)));
+    return otp;
+}
+
+// verify OTP
+verifyButtonRef.addEventListener('click', e => {
+    let inputVal = "";
+    inputRef.forEach(val => {
+        inputVal += val.value;
+    })
+    otpVerification(inputVal);
+})
+
+function otpVerification(inputVal){
+    if(String(inputVal)===String(generatedOTP)){
+        otpVerificationRef.style.display='flex';
+        rejectRef.style.display = "none";
+        successRef.style.display = "flex";
+        document.querySelector('.otp-verification').style.backgroundColor='rgba(0,0,0,0.5)';
+    }
+    else{
+        otpVerificationRef.style.display='flex';
+        successRef.style.display = "none";
+        rejectRef.style.display = "flex";
+        document.querySelector('.otp-verification').style.backgroundColor='rgba(0,0,0,0.5)';
+    }
+}
+
+otpVerificationRef.addEventListener('click' , e =>{
+    otpVerificationRef.style.display = 'none';
 })
